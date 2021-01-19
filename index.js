6c7d9b139f2f9f24b4ebd5638396877d6b6045c4
akbarjondev <tojiyev.akbarjon@gmail.com>
2021-01-19 12:48:27 +0500

    Make LinkedList class

--- index.js
+++ index.js
@@ -1,48 +1,4 @@
-class Node {
-
-	data
-	pointer
-
-	constructor(data) {
-		this.data = data
-		this.pointer = null
-	}
-}
-
-class LinkedList {
-
-	constructor() {
-		this.head = null
-	}
-
-	insert(data) {
-		const node = new Node(data)
-
-		if(!this.head) {
-			this.head = node
-		} else {
-
-			let tail = this.head
-
-			while(tail.pointer) {
-				tail = tail.pointer
-			}
-
-			tail.pointer = node
-		}
-	}
-
-	size() {
-		let counter = 1
-
-		while(this.head.pointer) {
-			this.head.pointer = this.head.pointer.pointer
-			counter++
-		}
-
-		return counter
-	}
-}
+const { LinkedList } = require('./LinkedList')
 
 const ll = new LinkedList()
 
@@ -50,6 +6,7 @@ ll.insert('A')
 ll.insert('B')
 ll.insert('C')
 ll.insert('E')
+ll.insert('F')
 ll.insert('F')
 
 console.log(ll.size())
